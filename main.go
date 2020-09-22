8c79c1faf55b4e52356b79369624e61c02cb5960
Rodrigo Valente <valentergs@gmail.com>
2020-09-22 15:15:36 -0300

    Encode example

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type person struct {
+	Nome string
+}
+
+func main() {
+	// p1 := person{
+	// 	Nome: "Rodrigo",
+	// }
+
+	// p2 := person{
+	// 	Nome: "Vanessa",
+	// }
+
+	// p3 := person{
+	// 	Nome: "Gustavo",
+	// }
+
+	// p4 := person{
+	// 	Nome: "Eduardo",
+	// }
+
+	// xp := []person{p1, p2, p3, p4}
+
+	// bs, err := json.Marshal(xp)
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
+
+	// fmt.Println(string(bs))
+
+	// xp2 := []person{}
+
+	// err = json.Unmarshal(bs, &xp2)
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
+
+	// fmt.Println(xp2)
+
+	http.HandleFunc("/encode", foo)
+	http.HandleFunc("/decode", bar)
+	http.ListenAndServe(":8080", nil)
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	p1 := person{
+		Nome: "Rodrigo",
+	}
+
+	err := json.NewEncoder(w).Encode(p1)
+	if err != nil {
+		log.Println("Encoded bad data")
+	}
+}
+
+func bar(w http.ResponseWriter, r *http.Request) {
+
+}
